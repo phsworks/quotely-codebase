3506e125f86acde558b75d880c81727e9395a54b
Peedee01 <piethein@schouten.nl>
2025-03-09 12:40:13 +0000

    9 Mar 2025, 13:40

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
-import {
-  Alert,
-  View,
-  ActivityIndicator,
-  StyleSheet,
-  TextInput,
-  Button,
-} from "react-native";
+import { useState, useEffect } from "react";
 import { supabase } from "../supabase/configUsers";
+import {
+  StyleSheet,
+  View,
+  Alert,
+  Text,
+  BackHandler,
+  TouchableOpacity,
+  TextInput
+} from "react-native";
+import { Button, Input } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
+import Feather from "@expo/vector-icons/Feather";
+import Avatar from "../components/Avatar";
 
 function ProfileScreen({ route }) {
   const [session, setSession] = useState(route.params?.session || null);
@@ -45,7 +50,7 @@ function ProfileScreen({ route }) {
 
       const { data, error, status } = await supabase
         .from("profiles")
-        .select("name, email, avatar_url")
+        .select("name, email", "avatar_url")
         .eq("id", session?.user.id)
         .single();
       if (error && status !== 406) throw error;
@@ -71,7 +76,7 @@ function ProfileScreen({ route }) {
         id: session?.user.id,
         name,
         email,
-        avatar_url: avatarUrl,
+        avatarUrl,
         updated_at: new Date(),
       };
 
@@ -84,7 +89,9 @@ function ProfileScreen({ route }) {
 
       // Update the auth table
       const { error: authError } = await supabase.auth.updateUser({
-        email,
+        data: {
+          email,
+        },
       });
 
       if (authError) throw authError;
@@ -100,45 +107,79 @@ function ProfileScreen({ route }) {
     }
   }
 
-  if (loading) {
-    return (
-      <View style={styles.loadingContainer}>
-        <ActivityIndicator size="large" color="#0000ff" />
-      </View>
-    );
-  }
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-        placeholder="Name"
-        editable={isEditing}
-      />
-      <TextInput
-        style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Email"
-        editable={isEditing}
-      />
-      <TextInput
-        style={styles.input}
-        value={avatarUrl}
-        onChangeText={setAvatarUrl}
-        placeholder="Avatar URL"
-        editable={isEditing}
-      />
-      {isEditing ? (
-        <Button
-          title="Save"
-          onPress={() => updateProfile({ name, email, avatarUrl })}
+      <View style={styles.profileTop}>
+        <Text style={styles.profileTitle}>Profile</Text>
+        <Avatar
+          size={150}
+          url={avatarUrl}
+          onUpload={(url) => {
+            setAvatarUrl(url);
+            updateProfile({ avatar_url: url });
+          }}
         />
-      ) : (
-        <Button title="Edit" onPress={() => setIsEditing(true)} />
-      )}
+      </View>
+      <View style={styles.editProfile}>
+        {isEditing ? (
+          <View style={styles.editingControl}>
+            <Feather
+              onPress={() => updateProfile({ name, email })}
+              name="check-circle"
+              size={28}
+              color="#545567"
+            />
+            <TouchableOpacity onPress={() => setIsEditing(false)}>
+              <Feather name="x-circle" size={28} color="#545567" />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <TouchableOpacity onPress={() => setIsEditing(true)}>
+            <Feather name="edit-3" size={28} color="#545567" />
+          </TouchableOpacity>
+        )}
+      </View>
+      <View style={styles.userTile}>
+        <Feather name="mail" size={24} color="#545567" />
+        {isEditing ? (
+          <Input
+            value={email || ""}
+            inputStyle={styles.input}
+            inputContainerStyle={styles.inputContainer}
+            onChangeText={(text) => setEmail(text)}
+          />
+        ) : (
+          <Text style={styles.userInfo}>{email}</Text>
+        )}
+      </View>
+      <View style={styles.userTile}>
+        <Feather name="user" size={24} color="#545567" />
+        {isEditing ? (
+          <Input
+            inputStyle={styles.input}
+            inputContainerStyle={styles.inputContainer}
+            value={name || ""}
+            onChangeText={(text) => setName(text)}
+          />
+        ) : (
+          <Text style={styles.userInfo}>{name}</Text>
+        )}
+      </View>
+      <View style={styles.userTile}>
+        <Feather name="settings" size={24} color="#545567" />
+        <Text style={styles.userInfo}>Settings</Text>
+      </View>
+
+      <View>
+        <TouchableOpacity
+          style={styles.userTile}
+          onPress={() => supabase.auth.signOut()}
+        >
+          <Feather name="log-out" size={24} color="#545567" />
+          <Text style={styles.userInfo}>Sign Out</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -147,22 +188,59 @@ export default ProfileScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 16,
+    marginTop: 0,
+    padding: 15,
   },
-  loadingContainer: {
-    flex: 1,
+  profileTop: {
     justifyContent: "center",
     alignItems: "center",
+    gap: 30,
+  },
+  profileTitle: {
+    fontSize: 28,
+    fontWeight: 600,
+    color: "#535360",
+  },
+  editProfile: {
+    paddingTop: 2,
+    marginTop: 10,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "flex-end",
+    paddingRight: 15,
+  },
+  editingControl: {
+    flexDirection: 'row',
+    gap: 10,
+  },
+  userTile: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 20,
+    boxShadow: "rgba(180, 182, 184, 0.402) 0px 4px 5px",
+    marginBottom: 20,
+    padding: 15,
+    borderRadius: 20,
+    backgroundColor: 'white'
+  },
+  userInfo: {
+    fontSize: 16,
+    color: "#434451",
+  },
+  inputContainer: {
+    width: "80%",
+    height: "1%",
+    borderBottomWidth: 0,
+    margin: 0,
+    marginTop: 20,
   },
   input: {
-    height: 40,
-    borderColor: "gray",
+    fontSize: 14, // Adjust the font size as needed
+    paddingHorizontal: 5,
     borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 8,
-    width: "100%",
+    borderColor: "#ccc",
+    borderRadius: 5,
+    color: "#333",
   },
 });
