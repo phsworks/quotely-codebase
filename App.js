8f90c8b935a5728104a3d635c17bc94383bd6420
Peedee01 <piethein@schouten.nl>
2025-06-11 15:28:24 +0200

    splash image deleted

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import MenuScreen from "./screens/MenuScreen";
 import { FavoritesQuotesProvider } from "./context/FavoritesContext";
 import QuoteDetailsScreen from "./screens/QuoteDetailsScreen";
 import InfoScreen from "./screens/InfoScreen";
-// import { MobileAds } from "react-native-google-mobile-ads";
+import { MobileAds } from "react-native-google-mobile-ads";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
